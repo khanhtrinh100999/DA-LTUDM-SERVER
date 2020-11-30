e1b259f6a94fb694c2a5c8614506fe1164e05953
Khanh Trinh <khanhneu99@gmail.com>
2020-11-30 10:27:45 +0700

    init project

---
+++ src/main/java/nuce/edu/daltudm/springdaltudm/SpringdaltudmApplication.java
@@ -0,0 +1,13 @@
+package nuce.edu.daltudm.springdaltudm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdaltudmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdaltudmApplication.class, args);
+	}
+
+}
